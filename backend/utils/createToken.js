1c855ea409b307b70f2d514404ea79de77511659
Md Danish Raza <md.danish0raza@gmail.com>
2025-07-01 21:45:08 +0530

    route fixes

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -7,7 +7,7 @@ export const createToken = (res, userId) => {
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production", // ensures HTTPS in production
-    sameSite: "Strict", // or 'Lax' depending on your needs
+    sameSite: "Strict", // for production
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30d
   });
 
